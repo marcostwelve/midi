03973b5de02252d61ba95201f9f84863f719475d
marcostwelve <marcostwelve>
2023-03-15 00:31:27 -0300

    Primeiro commit do Alura Midi

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+
+    else {
+        alert('Elemento não encontrado ou seletor inválido!');
+    }
+}
+
+for(contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento =  tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if(event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (event) {
+        if(event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.remove('ativa');
+        }
+    }
+}
